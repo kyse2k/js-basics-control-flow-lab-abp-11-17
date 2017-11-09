39941498dd64d896fe045fd6541be48629812129
Sérgio Pereira <sergiodanielpereira@gmail.com>
2017-11-09 00:03:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scuberGreetingForFeet(ride){
   if (ride <= 400){
     return "This one is on me!";
-  } else if (ride > 2001){
+  } else if (ride > 2000){
     return "I will gladly take your thirty bucks.";
   } else if (ride > 2500){
     return "No can do."
